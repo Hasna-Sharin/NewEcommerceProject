6ebc7d41c90fc6a5d8dcf0e3fa7098fdec84dc1e
Hasna-Sharin <sharinhasna29@gmail.com>
2024-12-11 10:57:34 +0530

    Add project files.

---
+++ NewEcommerceProject/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewEcommerceProject.Models;
+
+namespace NewEcommerceProject.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ NewEcommerceProject/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewEcommerceProject.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
